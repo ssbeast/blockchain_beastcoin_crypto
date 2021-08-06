9bbf6fce122f8d3226551fabb36b54d318d17395
ssbeast <ssbeast007@gmail.com>
2021-08-06 10:12:45 +0530

    add_transaction route added

--- beastcoin.py
+++ beastcoin.py
@@ -125,4 +125,14 @@ def is_valid():
         response = {'message': 'Oopsie, The Blockchain is not valid.'}
     return jsonify(response), 200
 
+@app.route('/add_transaction', methods = ['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
 app.run(host = '0.0.0.0', port = 5000)
