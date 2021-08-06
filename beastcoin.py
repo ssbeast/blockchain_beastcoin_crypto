6b24c99d5d86df2035c85bfe5f30dfe5032463d5
ssbeast <ssbeast007@gmail.com>
2021-08-06 10:23:47 +0530

    connect node route added

--- beastcoin.py
+++ beastcoin.py
@@ -135,4 +135,16 @@ def add_transaction():
     response = {'message': f'This transaction will be added to Block {index}'}
     return jsonify(response), 201
 
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The beastcoin Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
 app.run(host = '0.0.0.0', port = 5000)
